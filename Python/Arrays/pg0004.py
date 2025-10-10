c19a88f9315546d5aba0b44b7fc98b76c26d5240
BABIN JOE <babinjoe2945@gmail.com>
2025-10-10 13:13:50 +0530

    Create pg0004.py

---
+++ Python/Arrays/pg0004.py
@@ -0,0 +1,6 @@
+#Find missing number – In a sequence from 1 to n, detect which number is absent.
+
+n = int(input())
+arr = list(map(int,input().split()))
+total = n * (n + 1) // 2
+print(total - sum(arr))
